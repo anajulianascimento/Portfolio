4b5ade8a398064d8727defa0d157328cc4d09428
matheusnajal <matheusnajalk@gmail.com>
2024-12-29 23:13:30 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -10,7 +8,6 @@ function App() {
     <div>
       <h1>Oi cheirinhos!</h1>
       <p>Portfolio em construção!</p>
-      <p>OIIIII</p>
     </div>
   )
 }
